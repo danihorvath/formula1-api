48540dba1674bd99bb3ee3c815205fed9274a384
Daniel Horvath <hello@danihorvath.hu>
2024-07-04 00:14:39 +0200

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -7,6 +8,7 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
