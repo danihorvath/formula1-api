51f0f9d8ecbc82124e7e2f79bca55bbfcb47fa85
Daniel Horvath <hello@danihorvath.hu>
2024-07-03 20:08:27 +0200

    overtake endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -12,4 +12,22 @@ router.get("/drivers", (req: Request, res: Response) => {
   res.json(driversWithPlace);
 });
 
+router.post("/drivers/:driverId/overtake", (req: Request, res: Response) => {
+  const { driverId } = req.params;
+  const driver = driversWithPlace.find((d) => d.id === parseInt(driverId));
+
+  if (!driver) {
+    return res.status(404).json({ message: "Driver not found" });
+  } else if (driver.place === 1) {
+    return res.status(400).json({ message: "Driver is already first" });
+  }
+
+  const overtaken = driversWithPlace.find((d) => d.place === driver.place - 1)!;
+
+  driver.place -= 1;
+  overtaken.place += 1;
+
+  res.json({ message: "Overtake successful", newPositions: driversWithPlace });
+});
+
 module.exports = router;
